1c40ae1c94ecb7d9e861a0cae0d9d42bf52ac8d6
Maxime Daniel <root@maxux.net>
2015-06-22 14:12:55 +0200

    first version

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"flag"
 	"net/http"
+	"io/ioutil"
 	"github.com/gin-gonic/gin"
 	"github.com/garyburd/redigo/redis"
 	"encoding/json"
@@ -12,9 +14,9 @@ import (
 // data types
 //
 type CommandMessage struct {
-	Id   string   `json:"id"`
-	Gid  int      `json:"gid"`
-	Nid  int      `json:"nid"`
+	Id   int    `json:"id"`
+	Gid  int    `json:"gid"`
+	Nid  int    `json:"nid"`
 	// Cmd  string   `json:"cmd"`
 	// Args []string `json:"args"`
 	// Data string   `json:"id"`
@@ -23,12 +25,12 @@ type CommandMessage struct {
 //
 // redis stuff
 //
-func newPool() *redis.Pool {
+func newPool(addr string) *redis.Pool {
 	return &redis.Pool {
 		MaxIdle: 80,
 		MaxActive: 12000,
 		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", ":6379")
+			c, err := redis.Dial("tcp", addr)
 			
 			if err != nil {
 				panic(err.Error())
@@ -39,7 +41,7 @@ func newPool() *redis.Pool {
 	}
 }
 
-var pool = newPool()
+var pool *redis.Pool
 
 //
 // Command Reader
@@ -130,10 +132,31 @@ func log(c *gin.Context) {
 	gid := c.Param("gid")
 	nid := c.Param("nid")
 	
+	db := pool.Get()
+        defer db.Close()
+        
 	fmt.Printf("[+] gin: log (gid: %s, nid: %s)\n", gid, nid)
 	
-	// 
+	//
+	// read body
+	//
+	content, err := ioutil.ReadAll(c.Request.Body)
 	
+	if err != nil {
+		fmt.Println("[-] cannot read body:", err)
+		return
+	}
+	
+	//
+	// push body to redis
+	//
+	id := fmt.Sprintf("%s:%s:log", gid, nid)
+	fmt.Printf("[+] message destination [%s]\n", id)
+	
+	//
+	// push message to client queue
+	//
+	_, err = db.Do("RPUSH", id, content)
 	
 	//
 	c.JSON(http.StatusOK, "ok")
@@ -143,8 +166,45 @@ func result(c *gin.Context) {
 	gid := c.Param("gid")
 	nid := c.Param("nid")
 	
+	db := pool.Get()
+        defer db.Close()
+        
 	fmt.Printf("[+] gin: result (gid: %s, nid: %s)\n", gid, nid)
 	
+	//
+	// read body
+	//
+	content, err := ioutil.ReadAll(c.Request.Body)
+	
+	if err != nil {
+		fmt.Println("[-] cannot read body:", err)
+		return
+	}
+	
+	//
+	// decode body
+	//
+	var payload CommandMessage
+	err = json.Unmarshal(content, &payload)
+	
+	if err != nil {
+		fmt.Println("[-] cannot read json:", err)
+		return
+	}
+	
+	fmt.Printf("[+] payload: jobid: %d\n", payload.Id)
+	
+	//
+	// push body to redis
+	//
+	id := fmt.Sprintf("%d:%d:%d", payload.Gid, payload.Nid, payload.Id)
+	fmt.Printf("[+] message destination [%s]\n", id)
+	
+	
+	//
+	// push message to client queue
+	//
+	_, err = db.Do("RPUSH", id, content)
 	
 	//
 	c.JSON(http.StatusOK, "ok")
@@ -157,20 +217,29 @@ func stats(c *gin.Context) {
 	fmt.Printf("[+] gin: stats (gid: %s, nid: %s)\n", gid, nid)
 	
 	
+	
 	//
 	c.JSON(http.StatusOK, "ok")
 }
 
-func main() {	
+func main() {
+	ginPtr := flag.String("p", ":8080", "webservice listen addr:port")
+	redisPtr := flag.String("r", ":6379", "redis connect addr:port")
+	flag.Parse()
+	
+	fmt.Printf("[+] webservice: <%s>\n", *ginPtr)
+	fmt.Printf("[+] redis server: <%s>\n", *redisPtr)
+	
+	pool = newPool(*redisPtr)
 	router := gin.Default()
 	
 	go cmdreader()
 	
 	router.GET("/:gid/:nid/cmd", cmd)
-	router.GET("/:gid/:nid/log", log)
-	router.GET("/:gid/:nid/result", result)
+	router.POST("/:gid/:nid/log", log)
+	router.POST("/:gid/:nid/result", result)
 	router.GET("/:gid/:nid/stats", stats)
 	
 	
-	router.Run(":8080")
+	router.Run(*ginPtr)
 }
