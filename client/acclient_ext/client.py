b6c2c9fe0ee9e2257e27892e5da5afc2b325cf0e
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-07-15 14:42:21 +0200

    Adding passowrd to js ext

--- client/acclient_ext/client.py
+++ client/acclient_ext/client.py
@@ -2,8 +2,8 @@ import acclient
 
 
 class ACFactory(object):
-    def get(self, address='localhost', port=9999, db=0):
-        return acclient.Client(address, port, db)
+    def get(self, address='localhost', port=9999, password=None, db=0):
+        return acclient.Client(address, port, password, db)
 
     def runArgs(self, domain=None, name=None, max_time=0, max_restart=0, recurring_period=0, stats_interval=0,
                 args=None, loglevels='*', loglevels_db=None, loglevels_ac=None):
