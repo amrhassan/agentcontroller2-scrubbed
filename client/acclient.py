033b3cbe45bea96232b30798e795cc67933ec2da
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-07-15 13:14:38 +0200

    Add docstr to RunArgs

--- client/acclient.py
+++ client/acclient.py
@@ -21,7 +21,19 @@ class RunArgs(object):
     def __init__(self, domain=None, name=None, max_time=0, max_restart=0,
                  recurring_period=0, stats_interval=0, args=None, loglevels='*',
                  loglevels_db=None, loglevels_ac=None):
-        # init defaults.
+        """
+        :domain: Domain name
+        :name: script or executable name
+        :max_time: Max run time, 0 (forever), -1 forever but remember during reboots (long running),
+            other values is timeout
+        :max_restart: Max number of restarts if process died in under 5 min.
+        :recurring_period: Scheduling time
+        :stats_interval: How frequent the stats aggregation is done/flushed to AC
+        :args: Command line arguments (in case of execute)
+        :loglevels: Which log levels to capture and pass to logger
+        :loglevels_db: Which log levels to store in DB (overrides logger defaults)
+        :loglevels_ac: Which log levels to send to AC (overrides logger defaults)
+        """
         self._domain = domain
         self._name = name
         self._max_time = max_time
