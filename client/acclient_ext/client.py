745af1b88f2ab98926f39677f8eb60f2ab681ea7
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-07-15 08:20:12 +0200

    Process all log levels by default

--- client/acclient_ext/client.py
+++ client/acclient_ext/client.py
@@ -6,7 +6,7 @@ class ACFactory(object):
         return acclient.Client(address, port, db)
 
     def RunArgs(self, domain=None, name=None, max_time=0, max_restart=0, recurring_period=0, stats_interval=0,
-                args=None, loglevels=None, loglevels_db=None, loglevels_ac=None):
+                args=None, loglevels='*', loglevels_db=None, loglevels_ac=None):
         return acclient.RunArgs(domain=domain, name=name, max_time=max_time, max_restart=max_restart,
                                 recurring_period=recurring_period, stats_interval=stats_interval, args=args,
                                 loglevels=loglevels, loglevels_db=loglevels_db, loglevels_ac=loglevels_ac)
