7f770510e185264e6e86132adb1cdc0184b5b717
Amr Hassan <amr.hassan@gmail.com>
2015-11-02 17:59:31 +0200

    core: Updated incoming interface

--- core/incoming.go
+++ core/incoming.go
@@ -5,4 +5,7 @@ type Incoming interface {
 
 	// Receives and returns a single command without blocking, returning nil if there weren't any
 	ReceiveCommand() (*Command, error)
+
+	// Checks if the error is about a badly formatted incoming command
+	IsCommandFormatError(err error) bool
 }
